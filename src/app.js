229c69c0ae6ec1e52d8dfaa8ffd89effa9423d0f
nicolasviruel <nicolasviruel@gmail.com>
2023-09-06 04:33:06 -0300

    Se agregan Join con tablas intermedias, con relacion de (1 a varios) Sequelize crea la llave esporanea automaticamente User - Post, y ademas una relacion de (varios a varios), se necesita una tabla intermedia Page - User

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const moment = require("moment");
-const {User} = require("./db");
+const {User, Post, Page} = require("./db");
 
 const server= express();
 //inicializamos morgan y parceamos a json
@@ -11,14 +11,26 @@ server.use(morgan("dev"));
 //trae todos los usuarios o si le especifico alguno
 server.get("/users", async (req, res) =>{
     try {
-        const { name } = req.query;
+        const { name, last_name } = req.query;
         if (!name) {
-            const users = await User.findAll();
+            //si quiero restringuir y ver solo algunos elementos (utilizo los attributes)
+            const users = await User.findAll({ 
+                attributes: ["id" , "name", "last_name"],
+                include: [
+                    {
+                        model: Page,
+                        attributes: ["page_name"],
+                    },
+                ],
+             });
+            //si quiero excluir algun campo seria asi
+            // const users = await User.findAll({ attributes: { exclude: ["birth"]  }});
             return res.status(200).send(users);
         }else{
             const users = await User.findAll({
                 where:{
-                    name:name
+                    name:name,
+                    last_name: last_name
                 }
             });
             return res.status(200).send(users);
@@ -28,6 +40,29 @@ server.get("/users", async (req, res) =>{
         res.status(400).send({msg:"No se encontraron usuarios"});
     }
 })
+
+server.get("/users/findOrCreate", async (req, res) => {
+    try {
+      const { name, last_name, birth } = req.query; // Obtenemos los parámetros de consulta
+      const [user, created] = await User.findOrCreate({
+        where: { name },
+        defaults: {
+          last_name,
+          birth,
+        },
+      });
+  
+      if (created) {
+        res.status(201).send({ msg: "Usuario creado correctamente", user });
+      } else {
+        res.status(200).send({ msg: "Usuario encontrado", user });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(400).send({ msg: "Error al buscar o crear el usuario" });
+    }
+});
+  
 
 //Trae un solo usuario por su primaryKey
 server.get("/users/:id", async (req, res) =>{
@@ -46,12 +81,14 @@ server.get("/users/:id", async (req, res) =>{
 server.post("/users" , async (req, res) =>{
     //crear un usuario en la BD
     try {
-        const { name , last_name, birth } = req.body;
+        const { name , last_name, birth, pages } = req.body;
         //utilizamos moment para la fecha 
         const formattedBirth = moment(birth, "DD/MM/YYYY").format("YYYY-MM-DD");
         const newUser = await User.create({ name, last_name, birth: formattedBirth });
+        await newUser.addPages(pages);
         res.status(200).send(newUser);
     } catch (error) {
+        console.log(error);
         res.status(400).send({msg:"Error al crear el usuario"});
     }
 });
@@ -80,5 +117,37 @@ server.delete("/users/:id" , async (req, res) =>{
         res.status(400).send({msg:"Error al eliminar el usuario"});
     }
 });
+
+
+///////// ------------- POST ----------- /////////////
+
+
+//por mas que el modelo no lo tenga para crear un post necesito un Usuario
+server.post("/posts", async (req, res) =>{
+    try {
+        const {title , contents , userId} = req.body;
+        const newPost = await Post.create( {title, contents } ); 
+        await newPost.setUser(userId); //el metodo setUser indica de ese post cual es su User
+        res.status(200).send(newPost);
+        //para relalcionar de un usuario VARIOS post
+        // const user = await User.findByPk(userId)
+        // user.addPost([2,3,4])
+
+    } catch (error) {
+        res.status(400).send({msg:"Error al eliminar el usuario"});
+    }
+})
+
+//creador de paginas
+server.post("/pages", async (req, res) =>{
+    try {
+        const data = req.body;
+        const newPage = await Page.bulkCreate(data);
+        res.status(200).send(newPage);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({msg:"Error al crear las paginas"});
+    }
+})
 
 module.exports = server;
--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
-const {Sequelize, DataTypes} = require("sequelize");
+const {Sequelize} = require("sequelize");
+const UserFunc = require("./models/user");
+const PostFunc = require("./models/Post");
+const PageFunc = require("./models/Page");
+//
 const user = "postgres";
 const pass = "pichones1";
-const User = require("./models/user");
-const Post = require("./models/Post");
-
 const dbname = "lecture";
+
 //recordar que postgres conecta al puerto 5432
 const dataBase = new Sequelize(
     `postgres://${user}:${pass}@localhost:5432/${dbname}`,
@@ -12,9 +14,22 @@ const dataBase = new Sequelize(
     { logging: false }
 );
 
-User(dataBase);
-Post(dataBase);
+UserFunc(dataBase);
+PostFunc(dataBase);
+PageFunc(dataBase);
 
 console.log(dataBase.models);
+//para establecer las relaciones entre los modelos desestructuramos
+const { User, Post , Page} = dataBase.models;
+
+//relacion de (1 a varios) Sequelize crea la llave esporanea automaticamente
+User.hasMany(Post);
+Post.belongsTo(User);
+
+//relacion de (varios a varios), se necesita una tabla intermedia
+User.belongsToMany(Page, {through: "UserPage"} );
+Page.belongsToMany(User, {through: "UserPage"} );
+
+
 //mandamos los modelos definidos por la database(que tiene los modelos)
 module.exports = { dataBase, ...dataBase.models};
